43557663f0d9eda6c7faa595c0910263aabbb089
José Marrucho <marruchinho18@gmail.com>
2026-07-10 21:25:21 +0100

    alteração de alturas dos gregos
    Co-authored-by: pedarcalourenco <pedarcalourenco@users.noreply.github.com>

--- aulaiq/src/components/landing/ProductShowcase.tsx
+++ aulaiq/src/components/landing/ProductShowcase.tsx
@@ -6,13 +6,13 @@ export default function ProductShowcase() {
       <PhilosopherWatermark
         src="/images/philosophers/socrates.png"
         name="ΣΩΚΡΑΤΗΣ"
-        className="left-4 2xl:left-10 top-[-92px]"
+        className="left-4 2xl:left-10 top-[-95px]"
         imgClassName="w-[224px] 2xl:w-[269px]"
       />
       <PhilosopherWatermark
         src="/images/philosophers/athena.png"
         name="ΑΘΗΝΑ"
-        className="right-4 2xl:right-10 top-[-92px]"
+        className="right-4 2xl:right-10 top-[-90px]"
         imgClassName="w-[224px] 2xl:w-[269px]"
       />
 
